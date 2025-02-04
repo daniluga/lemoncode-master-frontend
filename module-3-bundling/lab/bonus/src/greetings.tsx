220a42b9d3cb43bfb0f6dc80f88d936a0ac29244
Daniel <daniluga97@gmail.com>
2025-02-04 22:41:35 +0100

    Upload module 3 lab: bonus exercise.

---
+++ module-3-bundling/lab/bonus/src/greetings.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import "./styles.scss";
+
+export const Greetings: FC = () => {
+  return (
+    <div className="greetings">
+      <h2>🍋 Welcome Lemoncoder! 🍋</h2>
+      <p> This is your Vite + React seed </p>
+      <p> Do whatever you want however you want </p>
+      <p> But please, learn and have fun! </p>
+    </div>
+  );
+};
